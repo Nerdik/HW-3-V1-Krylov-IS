afb358ba3ef1fc3a0e147e478e956e8d442d686c
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-09 11:23:12 +0300

    excercises 1 and 2 are completed

--- task.cpp
+++ task.cpp
@@ -1,1 +1,106 @@
 // Variant 1
+
+#include <iostream>
+#include <cmath>
+
+
+// Function for Excercise 1
+void ex1()
+{
+    float x, y;
+    std::cout << "Input x: ";
+    std::cin >> x;
+    std::cout << "Input y: ";
+    std::cin >> y;
+
+    if ((x * x + y * y < 9) && (y > 0))
+    {
+        std::cout << "Yes";
+    }
+    else if ((x * x + y * y > 9) || (y < 0))
+    {
+        std::cout << "No";
+    }
+    else 
+    {
+        std::cout << "On border";
+    }
+}
+
+// Function for Excercise 2
+void ex2()
+{
+    int num;
+    std::cout << "Input month number: ";
+    std::cin >> num;
+    switch (num)
+    {
+        case 1:
+            std::cout << "January";
+            break;
+        case 2:
+            std::cout << "February";
+            break;
+        case 3:
+            std::cout << "March";
+            break;
+        case 4:
+            std::cout << "April";
+            break;
+        case 5:
+            std::cout << "May";
+            break;
+        case 6:
+            std::cout << "June";
+            break;
+        case 7:
+            std::cout << "Jule";
+            break;
+        case 8:
+            std::cout << "August";
+            break;
+        case 9:
+            std::cout << "September";
+            break;
+        case 10:
+            std::cout << "October";
+            break;
+        case 11:
+            std::cout << "November";
+            break;
+        case 12:
+            std::cout << "December";
+            break;
+        default:
+            std::cout << "Input error. Try again!";
+    }
+}
+
+// Function for Excercise 3
+
+
+// Function for Excercise 4
+
+
+// Function for Excercise 5
+
+
+int main()
+{
+    // Exercise 1.
+    ex1();
+
+    // Exercise 2.
+    ex2();
+
+    // Exercise 3.
+
+
+    // Exercise 4.
+
+
+    // Exercise 5.
+
+
+    return 0;
+}
